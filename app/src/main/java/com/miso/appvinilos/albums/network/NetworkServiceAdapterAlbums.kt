caadae38213f8e507df81c349b6de903f83d6c38
mar-alas <m.alas@uniandes.edu.co>
2024-04-25 15:09:38 -0700

    Feature/hu02 consultar album (#68)
    * Agregando NetworkServiceAdapterAlbum
    
    * Agregando conexion con el backend y prueba
    
    * Actualizar el NetworkServiceAdapterAlbums para retornar una lista de Albums
    
    * Agregando el AlbumViewModel y AlbumRepository, actualizando el serviceAdapter y el MainActivity

--- app/src/main/java/com/miso/appvinilos/albums/network/NetworkServiceAdapterAlbums.kt
+++ app/src/main/java/com/miso/appvinilos/albums/network/NetworkServiceAdapterAlbums.kt
@@ -1,3 +1,27 @@
 package com.miso.appvinilos.albums.network
+import com.miso.appvinilos.albums.model.Album
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 
-// class NetworkServiceAdapter {}
+private const val BASE_URL =
+      "http://34.27.239.238:3000/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NetworkServiceAdapterAlbums {
+    @GET("albums")
+    suspend fun getAlbums(): List<Album>
+
+    @GET("albums/{albumId}")
+    suspend fun getAlbum(@Path("albumId") albumId: Int): Album
+}
+
+object AlbumsApi {
+    val albumsService : NetworkServiceAdapterAlbums by lazy {
+        retrofit.create(NetworkServiceAdapterAlbums::class.java) }
+}
--- app/src/main/java/com/miso/appvinilos/albums/repositories/AlbumRepository.kt
+++ app/src/main/java/com/miso/appvinilos/albums/repositories/AlbumRepository.kt
@@ -1,3 +1,15 @@
 package com.miso.appvinilos.albums.repositories
+import com.miso.appvinilos.albums.model.Album
+import com.miso.appvinilos.albums.network.AlbumsApi
+import retrofit2.Call
+class AlbumRepository {
+    private val albumService = AlbumsApi.albumsService
 
-// class AlbumRepository {}
+    suspend fun getAlbums(): List<Album> {
+        return albumService.getAlbums()
+    }
+
+    suspend fun getAlbum(albumId: Int): Album {
+        return albumService.getAlbum(albumId)
+    }
+}
