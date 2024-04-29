0ac3d7ef1ebeb2458ecaf17c3a9c682145ec00b6
Robert Castro <ra.castro2@uniandes.edu.co>
2024-04-28 21:49:54 -0500

    Refactor/estructura carpetas (#80)
    * Refactor estructura de carpetas
    
    * Refactor carpetas album

---
+++ app/src/main/java/com/miso/appvinilos/presentacion/ui/views/albumlist/AlbumItem.kt
@@ -0,0 +1,48 @@
+package com.miso.appvinilos.albums.ui.views
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.miso.appvinilos.data.model.Album
+import com.skydoves.landscapist.glide.GlideImage
+
+
+@Composable
+fun AlbumItem(album: Album) {
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .width(176.dp)
+            .clipToBounds()
+    ) {
+        GlideImage(
+            imageModel = { album.cover },
+
+            modifier = Modifier
+                .height(185.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
+        )
+        Text(
+            text = album.name,
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+        )
+        Text(
+            text = album.genre,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
---
+++ app/src/main/java/com/miso/appvinilos/presentacion/ui/views/albumlist/AlbumList.kt
@@ -0,0 +1,39 @@
+package com.miso.appvinilos.albums.ui.views
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.miso.appvinilos.albums.viewmodels.AlbumViewModel
+
+
+@Composable
+fun AlbumList(viewModel: AlbumViewModel, navigationController: NavHostController) {
+    val albums by viewModel.albums.observeAsState(initial = listOf())
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        content = {
+            items(albums) { album ->
+
+                Box(modifier=Modifier.fillMaxSize().clickable {
+                    navigationController.navigate("AlbumCompleteDetail/" + album.id)
+                }){
+                    AlbumItem(album)
+                }
+
+            }
+        },
+        modifier= Modifier.testTag("AlbumList")
+    )
+}
