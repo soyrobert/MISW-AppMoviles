f1f077cae4ae5ac7428ed865497d0bd8e113c209
DaGamez <da.gamez96@uniandes.edu.co>
2024-05-02 13:08:47 -0500

    se agrega la clase Artista al modelo

---
+++ app/src/main/java/com/miso/appvinilos/data/model/Artist.kt
@@ -0,0 +1,9 @@
+package com.miso.appvinilos.data.model
+
+data class Artist (
+    val id: Int,
+    val name:String,
+    val image:String,
+    val description:String,
+    val birthDate:String
+)
