1ba85cf281c723561b2e79239a1c402b99a96cbb
Jhon Freddy Puentes <jhonpuentes@MacBook-Pro-de-usuario.local>
2024-05-18 18:18:12 -0500

    test(accesibilidad): Implementacion de unit test para artistas y coleccionistas.

---
+++ app/src/androidTest/java/com/miso/appvinilos/accesibiliad/CollectorItemAccessibilityTest.kt
@@ -0,0 +1,81 @@
+package com.miso.appvinilos.accesibiliad
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.miso.appvinilos.data.model.Album
+import com.miso.appvinilos.data.model.Artist
+import com.miso.appvinilos.data.model.Collector
+import com.miso.appvinilos.data.model.Comment
+import com.miso.appvinilos.presentacion.ui.views.collectorlist.CollectorItem
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CollectorItemAccessibilityTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp(){
+        composeTestRule.setContent {
+            CollectorItem(collector = collector())
+        }
+    }
+
+    @Test
+    fun collectorItem_accessibility() {
+        composeTestRule.onNodeWithContentDescription(collector().name).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Artistas favoritos").assertIsDisplayed()
+    }
+
+    private fun collector(): Collector {
+        return Collector(
+            id = 1,
+            name = "John Doe",
+            telephone = "123456789",
+            email = "johndoe@example.com",
+            comments = listOf(
+                Comment(id = 1, content = "Great collector!"),
+                Comment(id = 2, content = "Always prompt and professional.")
+            ),
+            favoritePerformers = listOf(
+                Artist(
+                    id = 1,
+                    name = "Artist1",
+                    image = "image1",
+                    description = "description1",
+                    birthDate = "birthDate1"
+                ),
+                Artist(
+                    id = 1,
+                    name = "Artist2",
+                    image = "image2",
+                    description = "description2",
+                    birthDate = "birthDate2"
+                )
+            ),
+            collectorAlbums = listOf(
+                Album(
+                    id = 1,
+                    name = "Album1",
+                    cover = "cover1",
+                    releaseDate = "2021-01-01",
+                    description = "description1",
+                    genre = "genre1",
+                    recordLabel = "recordLabel1"
+                ),
+                Album(
+                    id = 1,
+                    name = "Album2",
+                    cover = "cover2",
+                    releaseDate = "2021-01-01",
+                    description = "description2",
+                    genre = "genre2",
+                    recordLabel = "recordLabel2"
+                )
+            )
+        )
+    }
+
+}
