1b5411cfd2fffee66c80fa47638b6a152a92fe5d
mar-alas <m.alas@uniandes.edu.co>
2024-05-23 06:07:39 -0700

    Agregando unit test crear album (#113)
    - Funcionalidad crear nuevo album terminado.
    - Pruebas unitarias funcionales, pruebas end to end, pruebas anteriores de accesibilidad pasando.
    - El feature funciona perfectamente. Fue probado en dispositivo samsung real.

--- app/src/main/java/com/miso/appvinilos/presentacion/ui/views/albumCreate/AlbumCreate.kt
+++ app/src/main/java/com/miso/appvinilos/presentacion/ui/views/albumCreate/AlbumCreate.kt
@@ -1,18 +1,30 @@
 package com.miso.appvinilos.presentacion.ui.views.albumCreate
-
+import android.app.DatePickerDialog
 import android.os.Build
 import android.util.Log
 import android.util.Patterns
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -27,6 +39,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -36,13 +51,13 @@ import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
+import java.util.Calendar
+import java.util.Date
 
 @RequiresApi(Build.VERSION_CODES.O)
 fun validateAndConvertDate(dateString: String): String? {
     val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-//    val outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'00:00:00XXX")
     val outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'00:00:00XX")
-//    2011-08-01T00:00:00-05:00
     return try {
         val localDate = LocalDate.parse(dateString, inputFormatter)
         val zonedDateTime = localDate.atStartOfDay(ZoneId.of("UTC-05:00"))
@@ -56,12 +71,13 @@ fun validateUrl(url: String): Boolean {
     return Patterns.WEB_URL.matcher(url).matches()
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AlbumCreate(viewModel: AlbumViewModel, navigationController: NavHostController) {
     var name by rememberSaveable { mutableStateOf("") }
     var cover by rememberSaveable { mutableStateOf("") }
-    var genre by rememberSaveable { mutableStateOf("") }
-    var recordLabel by rememberSaveable { mutableStateOf("") }
+    var genre by rememberSaveable { mutableStateOf("Classical") }
+    var recordLabel by rememberSaveable { mutableStateOf("Sony Music") }
     var description by rememberSaveable { mutableStateOf("") }
     var releaseDate by remember { mutableStateOf("") }
     val context = LocalContext.current
@@ -117,82 +133,18 @@ fun AlbumCreate(viewModel: AlbumViewModel, navigationController: NavHostControll
 
         Spacer(modifier = Modifier.padding(2.dp))
 
-        var isGenreError by remember { mutableStateOf(false) }
-        var genreErrorMessage by remember { mutableStateOf("") }
-        val validGenreLabels = listOf("Classical", "Salsa", "Rock", "Flok")
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = genre,
-            onValueChange = {
-                genre = it
-                if (genre in validGenreLabels) {
-                    isGenreError = false
-                    genreErrorMessage = ""
-                } else {
-                    isGenreError = true
-                    genreErrorMessage = "Opciones validas: Classical, Salsa, Rock, Flok"
-                }
-            },
-            placeholder = { Text(text = "Compañía Discografica") },
-            isError = isGenreError
-        )
-
-        if (isGenreError) {
-            Text(genreErrorMessage, color = MaterialTheme.colorScheme.error)
-        }
+        GenreDropdownMenuBox{ genre = it}
+        Log.d("OPCION", "Opcion: $genre")
 
         Spacer(modifier = Modifier.padding(2.dp))
 
-        var isDateValid by remember { mutableStateOf(true) }
-        TextField(
-            value = releaseDate,
-            onValueChange = {
-                releaseDate = it
-                val formattedDate = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    validateAndConvertDate(releaseDate)
-                } else {
-                    TODO("VERSION.SDK_INT < O")
-                }
-                if (formattedDate != null) {
-                    releaseDate = formattedDate
-                    isDateValid = true
-                } else {
-                    isDateValid = false
-                }
-            },
-            label = { Text("Release Date") },
-            isError = !isDateValid
-        )
-
-        if (!isDateValid) {
-            Text("Agregar un formato valido de fecha yyyy-MM-dd", color = MaterialTheme.colorScheme.error)
-        }
+        RecordLabelDropdownMenuBox { recordLabel = it}
+        Log.d("OPCION", "RECORD: $recordLabel")
 
         Spacer(modifier = Modifier.padding(2.dp))
 
-        var isRecordError by remember { mutableStateOf(false) }
-        var recordErrorMessage by remember { mutableStateOf("") }
-        val validLabels = listOf("Sony Music", "EMI", "Discos Fuentes", "Elektra", "Fania Records")
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = recordLabel,
-            onValueChange = {
-                recordLabel = it
-                if (recordLabel in validLabels) {
-                    isRecordError = false
-                    recordErrorMessage = ""
-                } else {
-                    isRecordError = true
-                    recordErrorMessage = "Opciones validas: Sony Music, EMI, Discos Fuentes, Elektra, Fania Records"
-                }
-            },
-            placeholder = { Text(text = "Compañía Discografica") },
-            isError = isRecordError
-        )
+        ReleaseDateTextField { releaseDate = it }
 
-        if (isRecordError) {
-            Text(recordErrorMessage, color = MaterialTheme.colorScheme.error)
-        }
         Spacer(modifier = Modifier.padding(2.dp))
 
         TextField(
@@ -204,14 +156,24 @@ fun AlbumCreate(viewModel: AlbumViewModel, navigationController: NavHostControll
         Spacer(modifier = Modifier.padding(2.dp))
 
         Button(onClick = {
-            val album = Album(
-                name = name, cover = cover,
-                releaseDate = releaseDate, description = description,
-                genre = genre, recordLabel = recordLabel
-            )
-            Log.d("Album", "Album: $album")
-            viewModel.createAlbum(album)
-        }) { Text("Create Album") }
+            if (isNameValid && isCoverValid && releaseDate.isNotBlank()
+                && description.isNotBlank() && genre.isNotBlank() && recordLabel.isNotBlank()){
+                val album = Album(
+                    name = name, cover = cover,
+                    releaseDate = releaseDate, description = description,
+                    genre = genre, recordLabel = recordLabel
+                )
+                Log.d("Album", "Album: $album")
+                viewModel.createAlbum(album) }
+                         },
+            enabled = (isNameValid && isCoverValid && releaseDate.isNotBlank()
+                    && description.isNotBlank() && genre.isNotBlank() && recordLabel.isNotBlank()),
+            colors = ButtonDefaults.buttonColors(
+                if (isNameValid && isCoverValid && releaseDate.isNotBlank() && description.isNotBlank() && genre.isNotBlank() && recordLabel.isNotBlank()) MaterialTheme.colorScheme.primary else Color.Gray
+            ),
+            modifier = Modifier
+                .testTag("SubmitAlbumButton")
+            ) { Text("Create Album") }
 
         albumCreationResponse?.let { response ->
             if (response.isSuccessful) {
@@ -221,6 +183,168 @@ fun AlbumCreate(viewModel: AlbumViewModel, navigationController: NavHostControll
                 }
             } else {
                 Text("Error al crear el album: ${response.message()}", color = Color.Red)
+            }
+        }
+    }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ReleaseDateTextField(releaseDate: (String) -> Unit) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed: Boolean by interactionSource.collectIsPressedAsState()
+
+    val currentDate = LocalDate.now().format(DateTimeFormatter.ISO_DATE)
+    var selectedDate by rememberSaveable { mutableStateOf(currentDate) }
+
+    val context = LocalContext.current
+
+    val calendar = Calendar.getInstance()
+    val year: Int = calendar.get(Calendar.YEAR)
+    val month: Int = calendar.get(Calendar.MONTH)
+    val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+    Date().also { calendar.time = it }
+
+    val datePickerDialog =
+        DatePickerDialog(context, { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            val localDate = LocalDate.of(year, month + 1, dayOfMonth)
+            selectedDate = localDate.format(DateTimeFormatter.ISO_DATE)
+            val formattedDate = validateAndConvertDate(selectedDate)
+            if (formattedDate != null) {
+                releaseDate(formattedDate)
+            }
+        }, year, month, day)
+
+    var isDateValid by remember { mutableStateOf(true) }
+
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .semantics { contentDescription = "Release Date TextField" },
+        readOnly = true,
+        value = selectedDate,
+        onValueChange = {
+            selectedDate = it // Update selected date
+            val formattedDate = validateAndConvertDate(it)
+            Log.d("Formatted", "Formatted Date: $formattedDate")
+            isDateValid = if (formattedDate != null) {
+                releaseDate(formattedDate)
+                true
+            } else {
+                false
+            }
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.DateRange,
+                contentDescription = "Date Range Icon",
+                modifier = Modifier.semantics { contentDescription = "Date Range Icon" }
+            )
+        },
+        interactionSource = interactionSource
+    )
+
+    if (!isDateValid) {
+        Text("Please enter a valid date", color = MaterialTheme.colorScheme.error)
+    }
+
+    if (isPressed) {
+        datePickerDialog.show()
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun GenreDropdownMenuBox(genre: (String) -> Unit) {
+    val context = LocalContext.current
+    val genres = arrayOf("Classical", "Salsa", "Rock", "Folk")
+    var expanded by remember { mutableStateOf(false) }
+    var genre by remember { mutableStateOf(genres[0]) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            TextField(
+                value = genre,
+                onValueChange = {},
+                label = { Text(text = "Seleccione un genero") },
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                genres.forEach { item ->
+                    DropdownMenuItem(
+                        text = { Text(text = item) },
+                        onClick = {
+                            genre = item
+                            expanded = false
+                            Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                            genre(item)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun RecordLabelDropdownMenuBox(recordLabel: (String) -> Unit) {
+    val context = LocalContext.current
+    val recordLabels = arrayOf("Sony Music", "EMI", "Discos Fuentes", "Elektra", "Fania Records")
+    var expanded by remember { mutableStateOf(false) }
+    var recordLabel by remember { mutableStateOf(recordLabels[0]) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            TextField(
+                value = recordLabel,
+                onValueChange = {},
+                label = { Text(text = "Seleccione una compañía discografica") },
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                recordLabels.forEach { item ->
+                    DropdownMenuItem(
+                        text = { Text(text = item) },
+                        onClick = {
+                            recordLabel = item
+                            expanded = false
+                            Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                            recordLabel(item)
+                        }
+                    )
+                }
             }
         }
     }
