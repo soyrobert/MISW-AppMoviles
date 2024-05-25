5de0bea3a39ec8ff9016e485f4586f2c400bcb67
Robert Castro <ra.castro2@uniandes.edu.co>
2024-05-25 18:56:38 -0500

    Fix iconos de comentarios album

--- app/src/androidTest/java/com/miso/appvinilos/pruebasunitarias/AddAlbum.kt
+++ app/src/androidTest/java/com/miso/appvinilos/pruebasunitarias/AddAlbum.kt
@@ -38,7 +38,7 @@ class AddAlbum {
         composeTestRule.onNodeWithText("Nombre").performTextInput("Test Album")
         composeTestRule.onNodeWithText("URL imagen portada").performTextInput("http://example.com/cover.jpg")
         composeTestRule.onNodeWithText("Descripción").performTextInput("This is a test album.")
-        composeTestRule.onNodeWithText("Crear Album").performClick()
+        composeTestRule.onNodeWithText("Agregar").performClick()
 
         // Verify the album creation
         composeTestRule.onNodeWithText("Test Album").assertIsDisplayed()
