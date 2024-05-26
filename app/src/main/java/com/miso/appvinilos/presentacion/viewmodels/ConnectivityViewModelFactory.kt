0636e87e8b1c797d22c95bf6f2bae68388fc5a9c
Daniel Alfonso Gamez Salcedo <da.gamez96@uniandes.edu.co>
2024-05-26 12:09:00 -0500

    ajustes micro optimizaciones (#123)
    Gracias

--- app/src/main/java/com/miso/appvinilos/presentacion/viewmodels/ConnectivityViewModelFactory.kt
+++ app/src/main/java/com/miso/appvinilos/presentacion/viewmodels/ConnectivityViewModelFactory.kt
@@ -1,11 +1,14 @@
 package com.miso.appvinilos.presentacion.viewmodels
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.miso.appvinilos.ConnectivityViewModel
 
 class ConnectivityViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ConnectivityViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
